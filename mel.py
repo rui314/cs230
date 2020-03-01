d0a3afab6fb0dbfd096f31cdd5438aff1dd5e822
Rui Ueyama <rui314@gmail.com>
2020-03-02 03:55:41 +0900

    temporary

--- mel.py
+++ mel.py
@@ -3,6 +3,9 @@ import librosa
 import soundfile
 
 y, sr = librosa.core.load('LibriSpeech/dev-clean/6241/66616/6241-66616-0010.flac')
-spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=512)
-y2 = librosa.feature.inverse.mel_to_audio(spectrogram, sr=sr, n_fft=512, n_iter=2048)
+spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=256, win_length=256, hop_length=64)
+y2 = librosa.feature.inverse.mel_to_audio(spectrogram, sr=sr, n_fft=256, win_length=256, hop_length=64)
+print(y.shape)
+print(spectrogram.shape)
+print(y2.shape)
 soundfile.write('out.wav', y2, sr)
