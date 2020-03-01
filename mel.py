7f09664895c2f428887890492ab7a9adfa0f5b68
Rui Ueyama <rui314@gmail.com>
2020-03-02 04:06:51 +0900

    temporary

--- mel.py
+++ mel.py
@@ -3,8 +3,8 @@ import librosa
 import soundfile
 
 y, sr = librosa.core.load('LibriSpeech/dev-clean/6241/66616/6241-66616-0010.flac')
-spectrogram = librosa.core.stft(y=y, win_length=512, hop_length=128)
-y2 = librosa.core.istft(spectrogram, win_length=512, hop_length=128)
+spectrogram = librosa.core.stft(y=y)
+y2 = librosa.core.istft(spectrogram)
 print(y.shape)
 print(spectrogram.shape)
 print(y2.shape)
