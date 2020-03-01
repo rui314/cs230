08fa8b12aad05fd53aa7cf65763a29cd80f59fdc
Rui Ueyama <rui314@gmail.com>
2020-03-02 03:56:29 +0900

    temporary

--- mel.py
+++ mel.py
@@ -3,8 +3,8 @@ import librosa
 import soundfile
 
 y, sr = librosa.core.load('LibriSpeech/dev-clean/6241/66616/6241-66616-0010.flac')
-spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=256, win_length=256, hop_length=64)
-y2 = librosa.feature.inverse.mel_to_audio(spectrogram, sr=sr, n_fft=256, win_length=256, hop_length=64)
+spectrogram = librosa.feature.stft(y=y, sr=sr, n_fft=256, win_length=256, hop_length=64)
+y2 = librosa.core.griffinlim(spectrogram, sr=sr, n_fft=256, win_length=256, hop_length=64)
 print(y.shape)
 print(spectrogram.shape)
 print(y2.shape)
