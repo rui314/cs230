12b7cbf597c42bb854e2e077939c01ea4c1055d2
Rui Ueyama <rui314@gmail.com>
2020-03-05 17:06:46 +0900

    temporary

--- train4.py
+++ train4.py
@@ -10,9 +10,9 @@ import tensorflow as tf
 import librosa
 import itertools
 from tensorflow import keras
-from tensorflow.keras.layers import Input, Dense, Conv2D, Conv2DTranspose, Conv1D, MaxPooling2D, UpSampling1D, UpSampling2D, Concatenate, Reshape, Dropout, LSTM, Activation, BatchNormalization, Add
+from tensorflow.keras.layers import Input, Dense, Conv2D, Conv2DTranspose, Conv1D, MaxPooling1D, MaxPooling2D, UpSampling1D, UpSampling2D, Concatenate, Reshape, Dropout, LSTM, Activation, BatchNormalization, Add, LeakyReLU
 from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger
 
 batch_size = 128
 initial_epoch = 0
@@ -60,54 +60,38 @@ def sample_generator(initial_epoch):
     
     while True:
         x = np.array(list(itertools.islice(it, batch_size)))
-        y = keras.utils.to_categorical(x, num_classes)
-
-        x = x.reshape((batch_size, num_samples))
-        y = y.reshape((batch_size, num_samples, num_classes))
-        yield x, y
+        x = x + 128
+        x = x.reshape((batch_size, num_samples, 1))
+        yield x, x
 
 # Create a keras model
 def get_model():
-    layers = 2
-    units = 5
+    layers = 1
+    units = 4
 
-    x = Input(shape=(num_samples))
-    y = Reshape((num_samples, 1))(x)
+    x = Input(shape=(num_samples, 1))
+    y = x
+
+    def block(y, i, s):
+        s += str(i)
+        y = Conv1D(2**(units+i), 15, padding='same', activation='relu', name=s+'_conv1d_1')(y)
+        y = Conv1D(2**(units+i), 15, padding='same', activation='relu', name=s+'_conv1d_2')(y)
+        return BatchNormalization(name=s+'_norm')(y)
 
     # Encoder
     for i in range(layers):
-        s = '_enc1_' + str(i)
-        y = Conv1D(2**(units+i), 15, strides=4, padding='same', name='conv1d'+s)(y)
-        y = Add()([Activation('tanh', name='tanh'+s)(y),
-                   Activation('sigmoid', name='sigmoid'+s)(y)])
-        y = BatchNormalization(name='norm'+s)(y)
+        y = block(y, i, 'enc')
+        y = MaxPooling1D(4)(y)
 
-        s = '_enc2_' + str(i)
-        y = Conv1D(2**(units+i), 15, padding='same', name='conv1d'+s)(y)
-        y = Add()([Activation('tanh', name='tanh'+s)(y),
-                   Activation('sigmoid', name='sigmoid'+s)(y)])
-        y = BatchNormalization(name='norm'+s)(y)
+    y = block(y, layers, 'enc')
 
     # Decoder
     for i in reversed(range(layers)):
         y = UpSampling1D(4)(y)
+        y = block(y, i, 'dec')
 
-        s = '_dec1_' + str(i)
-        y = Conv1D(2**(units+i-1), 15, padding='same', name='conv1d'+s)(y)
-        y = Add()([Activation('tanh', name='tanh'+s)(y),
-                   Activation('sigmoid', name='sigmoid'+s)(y)])
-        y = BatchNormalization(name='norm'+s)(y)
-
-        s = '_dec2_' + str(i)
-        y = Conv1D(2**(units+i-1), 15, padding='same', name='conv1d'+s)(y)
-        y = Add()([Activation('tanh', name='tanh'+s)(y),
-                   Activation('sigmoid', name='sigmoid'+s)(y)])
-        y = BatchNormalization(name='norm'+s)(y)
-
-    y = Dense(256, activation='tanh', name='tanh_final1')(y)
-    y = BatchNormalization(name='norm_final1')(y)
-    y = Dense(256, activation='tanh', name='tanh_final2')(y)
-    y = BatchNormalization(name='norm_final2')(y)
+    y = Dense(256, activation='relu', name='final_relu')(y)
+    y = Dense(256, activation='sigmoid', name='final_sigmoid')(y)
     y = Dense(256, activation='softmax', name='softmax_final')(y)
 
     return Model(inputs=x, outputs=y)
@@ -117,9 +101,11 @@ mirrored_strategy = tf.distribute.MirroredStrategy()
 with mirrored_strategy.scope():
     model = get_model()
 
-model.compile(keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
-model.summary()
+model.compile(keras.optimizers.Adam(0.01),
+              loss='sparse_categorical_crossentropy',
+              metrics=['sparse_categorical_accuracy'])
 
+model.summary()
 model.save('./model/saved_model')
 
 if len(sys.argv) == 2:
@@ -127,14 +113,17 @@ if len(sys.argv) == 2:
     model.load_weights(path, by_name=True)
     initial_epoch = int(re.match('.*/weights-(\d+).hdf5', path).group(1))
 
-checkpoint = ModelCheckpoint(filepath='./model/weights-{epoch:04d}.hdf5',
-                             verbose=0,
-                             save_best_only=False,
-                             save_weights_only=True,
-                             period=1)
+cp1 = ModelCheckpoint(filepath='./model/weights-{epoch:04d}.hdf5',
+                      verbose=0,
+                      save_best_only=False,
+                      save_weights_only=True,
+                      period=1)
+
+cp2 = ReduceLROnPlateau('loss')
+cp3 = CSVLogger('training.log')
 
 model.fit(x=sample_generator(initial_epoch),
           steps_per_epoch=100,
           initial_epoch=initial_epoch,
           epochs=135000,
-          callbacks=[checkpoint])
+          callbacks=[cp1, cp2, cp3])
